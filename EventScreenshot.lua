f16a7432867c0f14fd208573e8c028408b2316cd
camas <camas@hotmail.co.uk>
2018-03-19 12:51:14 +0000

    Add Ace libraries

---
+++ EventScreenshot.lua
@@ -0,0 +1,5 @@
+EventScreenshot = LibStub("AceAddon-3.0"):NewAddon("EventScreenshot", "AceConsole-3.0", "AceEvent-3.0")
+
+function EventScreenshot:OnInitialize()
+	Print("EventScreenshot enabled.")
+end
