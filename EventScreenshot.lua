e0ddb659a7218ab81404c96470221d406a161b49
camas <camas@hotmail.co.uk>
2018-03-19 19:48:04 +0000

    Add screenshot on achievement

--- EventScreenshot.lua
+++ EventScreenshot.lua
@@ -1,5 +1,16 @@
 EventScreenshot = LibStub("AceAddon-3.0"):NewAddon("EventScreenshot", "AceConsole-3.0", "AceEvent-3.0")
 
 function EventScreenshot:OnInitialize()
-	Print("EventScreenshot enabled.")
+	self:RegisterEvent("ACHIEVEMENT_EARNED")
+	self:Print("EventScreenshot enabled.")
+end
+
+function EventScreenshot:ACHIEVEMENT_EARNED(achvId)
+	local achvName = select(2, GetAchievementInfo(achvId))
+	self:TakeScreenshot("Achievement: " .. name)
+end
+
+function EventScreenshot:TakeScreenshot(message)
+	Screenshot()
+	self:Print("Took screenshot for " .. message)
 end
