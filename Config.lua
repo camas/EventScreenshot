7db45d788647e05260627c2ef136a3793f815af3
camas <camas@users.noreply.github.com>
2018-08-11 17:37:26 +0100

    Added config
    Change screenshot format/quality

---
+++ Config.lua
@@ -0,0 +1,90 @@
+local _, L = ...
+
+local SCREENSHOT_FORMAT_CVAR = "screenshotFormat"
+local SCREENSHOT_QUALITY_CVAR = "screenshotQuality"
+
+local currentFormat = GetCVar(SCREENSHOT_FORMAT_CVAR) or GetCVarDefault(SCREENSHOT_FORMAT_CVAR)
+local currentQuality = GetCVar(SCREENSHOT_QUALITY_CVAR) or GetCVarDefault(SCREENSHOT_QUALITY_CVAR)
+currentQuality = tonumber(currentQuality)
+
+L.optionsTable = {
+    type = "group",
+    args = {
+        general = {
+            type = "group",
+            name = "Screenshot Options",
+            order = 0,
+            inline = true,
+            args = {
+                format = {
+                    type = "select",
+                    name = "Format",
+                    desc = "The format WoW will use to save screenshots.",
+                    order = 0,
+                    values = {
+                        ["jpg"] = "JPG",
+                        ["tga"] = "TGA",
+                    },
+                    get = function() return currentFormat end,
+                    set = function(_, value)
+                        currentFormat = value
+                        SetCVar(SCREENSHOT_FORMAT_CVAR, value)
+                    end,
+
+                },
+                quality = {
+                    type = "range",
+                    name = "Quality",
+                    desc = "Quality of screenshots. 10 is highest.",
+                    order = 1,
+                    min = 1,
+                    max = 10,
+                    step = 1,
+                    get = function()
+                        if currentFormat == "tga" then
+                            return 10
+                        else
+                            return currentQuality
+                        end
+                    end,
+                    set = function(_, value)
+                       currentQuality = value 
+                       SetCVar(SCREENSHOT_QUALITY_CVAR, value)
+                    end,
+                    disabled = function() return currentFormat == "tga" end
+                },
+                screenshot_info = {
+                    type = "description",
+                    name = "Settings aren't saved to Config.wtf until the game is exited.",
+                    order = 2,
+                },
+            },
+        },
+        info = {
+            type = "group",
+            name = "Info",
+            order = 1,
+            inline = true,
+            args = {
+                addon_text = {
+                    type = "description",
+                    order = 0,
+                    name = ("%s v%s by %s"):format(_, GetAddOnMetadata(_, "Version"), GetAddOnMetadata(_, "Author")),
+                },
+                other_text = {
+                    type = "description",
+                    order = 1,
+                    name = 
+[[WoW has only two formats available for screenshots:
+TGA, a lossless format, meaning large file sizes but no quality reduction. (~7MB for 1080p)
+JPEG, which has variable compression, meaning lower file sizes but lower quality. (~3MB for 1080p at a quality of 10)
+
+By default WoW uses JPEG with a quality of 3.
+    
+Full source (10's of lines of code!!) available on GitHub @ https://github.com/camas/EventScreenshot
+Feedback welcome.]],
+                },
+            },
+        },
+    },
+}
--- EventScreenshot.lua
+++ EventScreenshot.lua
@@ -1,11 +1,27 @@
-EventScreenshot = LibStub("AceAddon-3.0"):NewAddon("EventScreenshot", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceBucket-3.0")
+local _, L = ...
 
-local ACHIEVEMENT_DELAY = 1.2
-local PRINT_DELAY = 0.2
+local EV_ADDON_NAME = "EventScreenshot"
+local EV_SHORT_NAME = "EV"
+local ACHIEVEMENT_DELAY = 1.2 -- Delay in seconds before screenshot taken after achievements
+local PRINT_DELAY = 0.2 -- Delay in seconds message printed to screen after screenshot.
+
+EventScreenshot = LibStub("AceAddon-3.0"):NewAddon(EV_ADDON_NAME, "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceBucket-3.0")
 
 function EventScreenshot:OnInitialize()
-	-- Achievement event bucketed so only one screenshot is taken for multiple achievements at the same time
+	-- Achievement event bucketed so only one screenshot is taken when multiple achievements earned at the same time.
 	self:RegisterBucketEvent({"ACHIEVEMENT_EARNED"}, ACHIEVEMENT_DELAY, "AchievementEarned")
+
+	-- Register config and add logo to it. (Logo code shamelessly appropriated from Immersion)
+	-- https://www.curseforge.com/wow/addons/immersion
+	LibStub("AceConfig-3.0"):RegisterOptionsTable(EV_ADDON_NAME, L.optionsTable)
+	L.configGUI = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(_)
+	local logo = CreateFrame('Frame', nil, L.configGUI)
+	logo:SetFrameLevel(4)
+	logo:SetSize(64, 64)
+	logo:SetPoint("TopRight", 4, 24)
+	logo:SetBackdrop({bgFile = ("Interface\\Addons\\%s\\Logo-64x64.tga"):format(_)})
+	L.configGUI.logo = logo
+
 	self:Print("enabled.")
 end
 
@@ -18,17 +34,15 @@ function EventScreenshot:AchievementEarned(achvIds)
 	if totalCount == 1 then
 		local key, value = next(achvIds, nil)
 		local _, achvName = GetAchievementInfo(key)
-		self:TakeScreenshot("Achievement: " .. achvName)
+		L.TakeScreenshot("Achievement: " .. achvName .. ".")
 	else
-		self:TakeScreenshot(totalCount .. " achievements.")
+		L.TakeScreenshot(totalCount .. " achievements.")
 	end
 end
 
 --- Takes a screenshot and prints a message to the console
 -- @param message The message to print
--- @param[opt=0] delay The delay in seconds before taking the screenshot.
-function EventScreenshot:TakeScreenshot(message, delay)
-	delay = delay or 0
+function L.TakeScreenshot(message)
 	Screenshot()
 	-- Delayed so doesn't show up in screenshot
 	self:DelayedPrint("Took screenshot for " .. message, PRINT_DELAY)
